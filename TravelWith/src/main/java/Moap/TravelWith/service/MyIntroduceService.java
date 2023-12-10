2138a2409050f81096c09e4e0e501757f6aeb679
MJ <alswns2631@cau.ac.kr>
2023-12-10 22:46:29 +0900

    hotfix4

--- TravelWith/src/main/java/Moap/TravelWith/service/MyIntroduceService.java
+++ TravelWith/src/main/java/Moap/TravelWith/service/MyIntroduceService.java
@@ -1,24 +1,26 @@
 package Moap.TravelWith.service;
 
 import Moap.TravelWith.dto.MyIntroduceDTO;
+import Moap.TravelWith.dto.MyPageDTO;
+import Moap.TravelWith.dto.match_response.MemberInfoDTO;
 import Moap.TravelWith.entity.Member;
 import Moap.TravelWith.entity.MyIntroduce;
 import Moap.TravelWith.repository.MemberRepository;
 import Moap.TravelWith.repository.MyIntroduceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class MyIntroduceService {
 
     private final MyIntroduceRepository myIntroduceRepository;
     private final MemberRepository memberRepository;
 
-    @Autowired
-    public MyIntroduceService(MyIntroduceRepository myIntroduceRepository, MemberRepository memberRepository) {
-        this.myIntroduceRepository = myIntroduceRepository;
-        this.memberRepository = memberRepository;
-    }
+
 
     public MyIntroduce createMyIntroduce(MyIntroduceDTO introduceDTO, Long memberId) {
         Member member = memberRepository.findById(memberId)
@@ -29,5 +31,28 @@ public class MyIntroduceService {
         myIntroduce.setContents(introduceDTO.getContents());
 
         return myIntroduceRepository.save(myIntroduce);
+    }
+
+    public MyPageDTO myPage(String email){
+
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() ->new IllegalArgumentException("Invalid member ID"));
+        Optional<MyIntroduce> myIntroduceOptional = myIntroduceRepository.findByMember(member);
+        MyIntroduce myIntroduce;
+        if (myIntroduceOptional.isEmpty()){
+            myIntroduce = new MyIntroduce();
+            myIntroduce.setMember(member);
+            myIntroduce.setTitle("Greetings!");
+            myIntroduce.setContents("I'm " + member.getName() + " and I want to travel With you!");
+        }
+        else{
+            myIntroduce = myIntroduceOptional.get();
+        }
+        MemberInfoDTO memberInfoDTO = MemberInfoDTO.entityToDto(member);
+        return MyPageDTO.builder().memberInfoDTO(memberInfoDTO)
+                .myIntroduceTitle(myIntroduce.getTitle())
+                .myIntroduceContents(myIntroduce.getContents()).build();
+
+
     }
 }
